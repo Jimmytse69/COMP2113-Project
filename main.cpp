aca3cf128bdd0888d3a753ddb8485e0421ea1c09
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2020-11-16 21:52:45 +0800

    update main.cpp (ncurses)
    -make it more concise
    -add do while loop for user input (with player.h)

--- main.cpp
+++ main.cpp
@@ -1,42 +1,43 @@
-//main.cpp for geting user input and inti map
+//reference to:
+//https://www.youtube.com/watch?v=A5lX1h_2zy0&list=PL2U2TQ__OrQ8jTf0_noNKtHMuYlyxQl4v&index=2
+//ncurses tutorials play list
 
 
-#include <iostream>
 #include <ncurses.h>
+#include <string>
+#include <cstdlib>
+#include <ctime> 
+#include "player.h"     //all player infomation(location) and control
 
 using namespace std;
 
-
 int main()
 {
-    initscr();  //initalize ncurses screen
+    initscr();          //initialize ncurses "terminal"
+    noecho();           //press key without printing it (no echo)
 
-    int height, width, start_y, start_x;
-    height = 30;
-    width = 30;
-    start_y = start_x = 0;
-    int p_coor[2] = {15,15};    //player spawn point
+    //get screen size
+    int yMax, xMax;
+    getmaxyx(stdscr, yMax, xMax);
 
-    WINDOW * win = newwin(height, width, start_y, start_x);   //create a new window(console) for game
+    //create new "window" to play
+    WINDOW *playwin = newwin(30, 50, 0, 0);
+    box(playwin, 0, 0);
     refresh();
+    wrefresh(playwin);      //refresh window to update changes
 
-    box(win, 0, 0);         //create a border of game zone
+    Player * p = new Player(playwin, 15, 25, '<');      //create pac-man
 
-    for (int i = 1; i < 29; ++i) {
-        for (int j = 1; j < 29; ++j) {
-            if (i == p_coor[0] && j == p_coor[1]) {
-                mvwprintw(win, i, j, "<");   //'<' denoted as player
-            }
-            else {
-                mvwprintw(win, i, j, ".");
-            }
-        }
-    }
-    wrefresh(win);        //"image" will display after window refresh
+    do {
+        p -> display();
+        wrefresh(playwin);
+    }   while (p -> getmv() != 'm');        //'m' to quit game
 
-    getch();
+
 
     endwin();
+
+    delete p;       //free memory
 
     return 0;
 }
