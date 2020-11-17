b3884bab558174d9b42e3d66a1af007f5f4ec4db
Wong Ming Yui Lucas <69389014+lucaswmy@users.noreply.github.com>
2020-11-17 17:49:06 +0800

    Update ghost.cpp

--- ghost.cpp
+++ ghost.cpp
@@ -1,9 +1,25 @@
 
 #include <ncurses.h>
+#include <iostream>
+#include <time.h>
+#include <vector>
 #include "ghost.h"
 
 //Rotate the ghost randomly when hit a wall
 //Angle: 90 degrees
-void ghost_move() {
-
+void ghost_speed() {
+  vector<ghost> Ghosts;
+  ghost gt1, gt2, gt3;
+  gt1.speed = 1;
+  gt2.speed = 1
+  gt3.speed = 1;
+  
+  cin >> fastly;
+  if      (fastly == "A")   { speed = 3; }
+  else if (fastly == "B")   { speed = 5; }
+  
 }
+
+void ghost_turn() {
+  
+
