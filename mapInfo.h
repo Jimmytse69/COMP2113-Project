e3a313e12fcd252724b15bb6df29ce52534b6258
Wong Ming Yui Lucas <69389014+lucaswmy@users.noreply.github.com>
2020-11-19 14:11:21 +0800

    Update mapInfo.h

--- mapInfo.h
+++ mapInfo.h
@@ -4,10 +4,15 @@
 class mapInfo{
     public:
         void init_dot(WINDOW * playwin);
-        void moving(dot);
-        
+    
         int directions[8] = {1,2,3,4,5,6,7,8};          
 
 };
+
+class dot {
+    public:
+        chtype Dot_o = 'o';
+        int x_coor, y_coor;
+}
 
 #endif
