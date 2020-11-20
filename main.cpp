b03ebd65595337128e630ea38a113bf733f6f9d7
Wong Ming Yui Lucas <69389014+lucaswmy@users.noreply.github.com>
2020-11-20 17:09:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -90,10 +90,9 @@ int main()
 
     //auto start = std::chrono::system_clock::now();  //count time begin
   
-    if (Dot.size() <= 3) {
-      map1 -> init_dot(playwin, Dot);}
-  
     do {
+        if (Dot.size() <= 3) {
+          map1 -> init_dot(playwin, Dot);}
         //auto t = std::chrono::system_clock::now();
         //mvwprintw(playwin, 1, 55, std::chrono::duration_cast<std::chrono::seconds>(end - start).count());
 
