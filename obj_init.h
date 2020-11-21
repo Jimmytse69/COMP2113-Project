d8fce2735bd5d29b092e877dd33dbf8379f47373
Wong Ming Yui Lucas <69389014+lucaswmy@users.noreply.github.com>
2020-11-21 23:14:27 +0800

    Update obj_init.h

--- obj_init.h
+++ obj_init.h
@@ -13,8 +13,8 @@ std::vector<int> obj_init(int y_coor, int x_coor)
 
 int obj_refresh(std::vector<int> occupied, int y_coor, int x_coor)  //indicated object (e.g. player occurpied that cooridinate, in terms of (y,x))
 {
-    // occupied.pop_back();
-    // occupied.pop_back();
+    occupied.pop_back();
+    occupied.pop_back();
 
     occupied.push_back(y_coor);
     occupied.push_back(x_coor);
