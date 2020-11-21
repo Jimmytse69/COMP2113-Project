ede7d7bc6baa8ff88c52ec1aa95ee0406de3e8df
Jimmy Tse <55787794+Jimmytse69@users.noreply.github.com>
2020-11-21 12:26:02 +0800

    cannot clear display
    -player(snake) can now change length, but cannot clear the privous printed "footprint".

--- obj_init.h
+++ obj_init.h
@@ -1,23 +1,35 @@
 #ifndef _OBJ_INIT_H
 #define _OBJ_INIT_H
 
+#include "player.h"
 
 std::vector<int> obj_init(int y_coor, int x_coor)
 {
-    std::vector<int> occupied;       
+    std::vector<int> occupied;
     occupied.push_back(y_coor);
     occupied.push_back(x_coor);
 
     return occupied;
 }
 
-int obj_refresh(std::vector<int> occupied, int y_coor, int x_coor)  //indicated object (e.g. player occurpied that cooridinate, in terms of (y,x))
+int obj_refresh(std::vector<int> &occupied, int y_coor, int x_coor, bool len_inc)  //indicated object (e.g. player occurpied that cooridinate, in terms of (y,x))
 {
-    occupied.pop_back();
-    occupied.pop_back();
+    if (len_inc == false){
+      occupied.pop_back();
+      occupied.pop_back();
+      occupied.push_back(y_coor);
+      occupied.push_back(x_coor);
+    }
+    else {
+      std::vector<int> copy;        //for len inc, need push first coor to head frist (becoz no push_front function)
+      copy.push_back(y_coor);
+      copy.push_back(x_coor);
+      for (int i = 0; i < occupied.size(); ++i) {
+        copy.push_back(occupied[i]);
+      }
+      occupied = copy;    //overwrite it to pass by ref
+    }
 
-    occupied.push_back(y_coor);
-    occupied.push_back(x_coor);
 
     return(occupied.size()/2);       //retunr number of obj
 }
