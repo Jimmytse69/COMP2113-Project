084b7facf53c4b18b9866d03335a1292db2ca8b9
Wong Ming Yui Lucas <69389014+lucaswmy@users.noreply.github.com>
2020-11-21 21:01:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -101,12 +101,11 @@ int main()
    
           map1 -> init_dot(playwin, Dot);}
       
-        obj_refresh(player_Location, p -> y_coor, p -> x_coor, len_inc);     //refresh player location, in obj_init.h
-        len_inc = true;
-      
         p -> display(player_Location);
         wrefresh(playwin);
-  
+      
+        obj_refresh(player_Location, p -> y_coor, p -> x_coor, len_inc);     //refresh player location, in obj_init.h
+        len_inc = true;
 
         if (p -> check_alive()) {
           end = true;
